53220298658c86f72dac495d76bb2348c2ca15d7
Thomas Colgrove <trcolgrove@gmail.com>
2015-11-05 13:36:06 -0500

    Added portals prototype unity project with hopefully clean git integration

---
+++ Portals/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () { 
+
+	}
+
+
+	void LateUpdate() {
+			transform.position = new Vector3(transform.position.x,
+		                                 transform.position.y,
+		                                 player.transform.position.z + offset.z);
+	}
+
+}
