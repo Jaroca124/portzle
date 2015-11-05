82b0962b0668d54077ebce84b6c2b1a427e3472e
Thomas Colgrove <trcolgrove@gmail.com>
2015-11-05 12:43:40 -0500

    basic portals prototype, including basic portal logic and marble phsyics

---
+++ Portals/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () { 
+		/*
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (0, 0, moveVertical);
+		transform.position += movement/10;*/
+	}
+
+
+	void LateUpdate() {
+	/*	transform.position = new Vector3(transform.position.x,
+		                                 transform.position.y,
+		                                 player.transform.position.z + offset.z);*/
+	}
+
+}
