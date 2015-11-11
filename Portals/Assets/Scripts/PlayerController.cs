f59b939ea146632a7447727748795e0338133a5b
Mike Yeung <mikeyeung123@gmail.com>
2015-11-11 12:22:11 -0500

    Can auto-generate platforms

--- Portals/Assets/Scripts/PlayerController.cs
+++ Portals/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 10; 
+	public float verticalSpeed = 1;
 	public float popOutY = 200;
 
 	private Rigidbody rb;
@@ -21,9 +22,8 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate () 
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		Vector3 movement = new Vector3 (moveHorizontal, 0, verticalSpeed);
 		rb.AddForce (movement * speed);
 	}
 
