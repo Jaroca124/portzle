61e50d063ea49b38f6be5c298a835beb1c2fc3a3
Samantha Welch <Samantha@resnet-130-64-145-42.medford.tufts.edu>
2015-12-06 18:38:17 -0500

    fixed snowflakes

--- Portals/Assets/StarBehavior.cs
+++ Portals/Assets/StarBehavior.cs
@@ -7,11 +7,14 @@ public class StarBehavior : MonoBehaviour {
 	public float tiltAngle = 90.0F;
 	public float scale = 0.5F;
 	void Start () {
+		this.gameObject.transform.Rotate (90, this.gameObject.transform.rotation.y,
+		                                 this.gameObject.transform.rotation.z);
 		
 	}
 	void Update() {
 		this.gameObject.transform.localScale = new Vector3(0.5F, 0.5F, 0.5F);
-		this.gameObject.transform.Rotate(0, 200*Time.deltaTime, 0);
+		this.gameObject.transform.Rotate(0, 0, 
+		                                 200*Time.deltaTime);
 	}
 
 	void OnTriggerEnter(Collider other){
