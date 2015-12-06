7bd44c6db12b67e5a329579614ab4018d8b14c9d
Jaroca124 <jacob.caccamo@tufts.edu>
2015-12-05 19:56:09 -0500

    adding instructions page

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ $(document).ready(function() {
         $('#overlay1').fadeIn(2000);
         $('#banner_text').delay(1000).fadeIn(2000);
 });
+
+function load() {
+    //Arrow Action
+    $('.goto-next').click(function(event) {
+        var status = $(this).attr('id').split('_')[1];
+        $('html, body').animate({
+            scrollTop: $('#' + status).offset().top
+        }, 500);
+    });
+
+    $('.goto-next-black').click(function(event) {
+        var status = $(this).attr('id').split('_')[1];
+        $('html, body').animate({
+            scrollTop: $('#' + status).offset().top
+        }, 500);
+    });
+}
