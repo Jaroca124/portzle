aa6aace776b3b58063c980801e4175bac33b852c
Thomas Colgrove <trcolgrove@gmail.com>
2015-12-10 12:29:57 -0500

    added resources to game

---
+++ Portals/Assets/Scripts/StarBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarBehavior : MonoBehaviour {
+
+	public float smooth = 100.0F;
+	public float tiltAngle = 90.0F;
+	public float scale = 0.5F;
+	void Start () {
+		
+	}
+	void Update() {
+		this.gameObject.transform.Rotate(0, 0, 
+		                                 200*Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		Destroy (this.gameObject);
+		//score += 5;
+	}
+}
