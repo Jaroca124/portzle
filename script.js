4e41943b4eaa057bc8e53c7782fddfeb12d7c940
Jaroca124 <jacob.caccamo@tufts.edu>
2015-12-11 20:13:35 -0500

    cleaning up the website

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
         $('#overlay1').fadeIn(2000);
         $('#banner_text').delay(1000).fadeIn(2000);
+        $('#githublink_button').delay(1000).fadeIn(2000);
 });
 
 function load() {
